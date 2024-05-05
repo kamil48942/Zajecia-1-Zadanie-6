bcadd8a72df168fceb73a0c0dae1e3edf8d93711
kamil48942 <kamil7167@wp.pl>
2024-05-05 15:40:09 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,17 +10,13 @@ class Program
 
         try
         {
-            // Otwieramy istniejący plik do odczytu
             using (FileStream wejsciowyPlik = new FileStream(sciezkaWejsciowegoPliku, FileMode.Open))
             {
-                // Tworzymy nowy plik do zapisu
                 using (FileStream wyjsciowyPlik = new FileStream(sciezkaWyjsciowegoPliku, FileMode.Create))
                 {
-                    // Tworzymy bufor do przechowywania odczytanych danych
                     byte[] bufor = new byte[1024];
                     int odczytaneBajty;
 
-                    // Odczytujemy dane z pliku wejściowego i zapisujemy je do pliku wyjściowego
                     while ((odczytaneBajty = wejsciowyPlik.Read(bufor, 0, bufor.Length)) > 0)
                     {
                         wyjsciowyPlik.Write(bufor, 0, odczytaneBajty);
